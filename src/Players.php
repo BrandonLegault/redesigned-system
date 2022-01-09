943552d26f45f038b0613c79a5d516a49299f2e3
Pax <13646646+paxcodes@users.noreply.github.com>
2022-01-08 16:44:26 -0800

    create FileSource

--- src/Players.php
+++ src/Players.php
@@ -26,53 +26,6 @@ class Players
         $this->dataSource = $dataSource;
     }
 
-    /**
-     * @param $source string Where we're retrieving the data from. 'json', 'array' or 'file'
-     * @param $filename string Only used if we're reading players in 'file' mode.
-     * @return string json
-     */
-    function readPlayers($source, $filename = null)
-    {
-        $playerData = null;
-
-        switch ($source) {
-            case 'file':
-                $playerData = $this->getPlayerDataFromFile($filename);
-                break;
-        }
-
-        if (is_string($playerData)) {
-            $playerData = json_decode($playerData);
-        }
-
-        return $playerData;
-    }
-
-    /**
-     * @param $source string Where to write the data. 'json', 'array' or 'file'
-     * @param $filename string Only used if we're writing in 'file' mode
-     * @param $player \stdClass Class implementation of the player with name, age, job, salary.
-     */
-    function writePlayer($source, $player, $filename = null)
-    {
-        switch ($source) {
-            case 'file':
-                $players = json_decode($this->getPlayerDataFromFile($filename));
-                if (!$players) {
-                    $players = [];
-                }
-                $players[] = $player;
-                file_put_contents($filename, json_encode($players));
-                break;
-        }
-    }
-
-    function getPlayerDataFromFile($filename)
-    {
-        $file = file_get_contents($filename);
-        return $file;
-    }
-
     function display(IFormatter $formatter)
     {
         $players = $this->dataSource->read();
